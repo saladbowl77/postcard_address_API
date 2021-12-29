3ca18f73b4d3238f6b6786222478c3690136b89c
saladbowl77 <sarakun.info@gmail.com>
2021-12-29 16:11:14 +0900

    fix : port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const port = 5000
+const port = process.env.PORT || 5000;
 const mkImg   = require('./mkImg.js');
 var fs = require('fs');
 
