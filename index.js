7848869d57539eb3579b05641590a9332d41c202
saladbowl77 <sarakun.info@gmail.com>
2021-12-29 16:01:11 +0900

    fix : port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const port = 8080
+const port = 5000
 const mkImg   = require('./mkImg.js');
 var fs = require('fs');
 
